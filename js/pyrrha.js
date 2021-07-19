7c51832760406159683820fef30df5d07fe85f4e
Daniel Krook <krook@us.ibm.com>
2021-07-19 18:40:30 -0400

    Mark readings with color.
    Signed-off-by: Daniel Krook <krook@us.ibm.com>

--- js/pyrrha.js
+++ js/pyrrha.js
@@ -41,17 +41,14 @@ const NO2_RED = 8;
     "use strict";
 
     function sendNotification(message) {
-        var appControl = new tizen.ApplicationControl('http://tizen.org/appcontrol/operation/create_content', null, 'image/png', null, null);
 
         var notificationGroupDict = {
             content: message,
             images: {
-                iconPath: '../icon.png'
+                iconPath: 'img/pyrrha-watch-icon.png'
             },
             actions: {
                 vibration: true,
-                /* Application control to be launched when the user selects the notification */
-                appControl: appControl
             }
         };
 
@@ -61,52 +58,53 @@ const NO2_RED = 8;
     }
 
     function setSensorValues() {
-    	
-    		// To simulate for local testing, set up some random values to display.
-    		/*
-        var co = [150, 200, 250, 300, 350, 400, 500];
-        var no2 = [0.05, 0.10, 0.15, 0.20, 0.25];
-        var tmp = [30, 31, 32, 33, 34, 35];
-        var hum = [82, 83, 85, 87, 89];
-        document.getElementById('co').innerText = co[Math.floor(Math.random() * co.length)];
-        document.getElementById('no2').innerText = no2[Math.floor(Math.random() * no2.length)];
-        document.getElementById('tmp').innerText = tmp[Math.floor(Math.random() * tmp.length)];
-        document.getElementById('hum').innerText = hum[Math.floor(Math.random() * hum.length)];
-        
-        document.getElementById('co').innerText = (Math.random() * 150).toFixed(2);
-        document.getElementById('no2').innerText = (Math.random() * 10).toFixed(2);
-        document.getElementById('tmp').innerText = (Math.random() * 50).toFixed(2);
-        document.getElementById('hum').innerText = (Math.random() * 100).toFixed(2);
-        	*/
-        
-        document.getElementById('co').innerText = (Math.random() * 150).toFixed(2);
-        document.getElementById('no2').innerText = (Math.random() * 10).toFixed(2);
-        document.getElementById('tmp').innerText = (Math.random() * 50).toFixed(2);
-        document.getElementById('hum').innerText = (Math.random() * 100).toFixed(2);
-        
-        	// TODO: Get real-time values from Bluetooth
 
-        	// Send notifications if readings are too high
-        if (parseInt(document.getElementById('co').innerText) >= CO_RED) {
-            sendNotification('CO too high.')
-        }
-       
-        if (parseInt(document.getElementById('no2').innerText) >= NO2_RED) {
-            sendNotification('NO2 too high.')
+        var readingCo = document.getElementById('reading-co');
+        var readingNo2 = document.getElementById('reading-no2');
+        var readingTmp = document.getElementById('reading-tmp');
+        var readingHum = document.getElementById('reading-hum');
+
+        // To simulate for local testing, set up some random values to display.
+        readingCo.innerText = (Math.random() * 150).toFixed(1);
+        readingNo2.innerText = (Math.random() * 10).toFixed(1);
+        readingTmp.innerText = (Math.random() * 50).toFixed(1);
+        readingHum.innerText = (Math.random() * 100).toFixed(1);
+
+        // TODO: Get real-time values from Bluetooth
+
+        // Send notifications if readings are too high
+        if (parseInt(readingCo.innerText) >= CO_RED) {
+            readingCo.className = 'color-red';
+            sendNotification('CO too high.');
+        } else {
+            readingCo.className = 'color-green';
         }
 
-        if (parseInt(document.getElementById('tmp').innerText) >= TMP_RED) {
-            sendNotification('Tmp too high.')
+        if (parseInt(readingNo2.innerText) >= NO2_RED) {
+            readingNo2.className = 'color-red';
+            sendNotification('NO2 too high.');
+        } else {
+        		readingNo2.className = 'color-green';
         }
 
-        if (parseInt(document.getElementById('hum').innerText) >= HUM_RED) {
-            sendNotification('Hum too high.')
+        if (parseInt(readingTmp.innerText) >= TMP_RED) {
+            readingTmp.className = 'color-red';
+            sendNotification('Tmp too high.');
+        } else {
+        		readingTmp.className = 'color-green';
         }
 
-        	// Refresh values every second
+        if (parseInt(readingHum.innerText) >= HUM_RED) {
+            readingHum.className = 'color-red';
+            sendNotification('Hum too high.');
+        } else {
+        		readingHum.className = 'color-green';
+        }
+
+        // Refresh values every second
         window.setTimeout(setSensorValues, 1000);
     }
-    
+
     setSensorValues();
 }());
 
